49959b73993d89b75a0bd13d0e8d74ab1441fb53
zhanzhangwei <921397102@qq.com>
2022-03-25 01:21:19 +0800

    [add]更新配置文件

---
+++ conf/common.py
@@ -0,0 +1,11 @@
+from .produce import *
+from .local import *
+
+IP = IP
+# mysql配置
+HOST = HOST
+PORT = PORT
+DATABASE = DATABASE
+USER = USER
+PASSWORD = PASSWORD
+
---
+++ conf/produce.py
@@ -0,0 +1,8 @@
+# mysql配置信息
+HOST = '127.0.0.1'
+PORT = '3306'
+DATABASE = 'kafka'
+USER = 'root'
+PASSWORD = '123456'
+
+IP = "127.0.0.1:9092"
